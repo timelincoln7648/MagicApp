299551cadaae393fb238f3f7ebac764dc7120379
Kody <timelincoln@gmail.com>
2015-10-13 15:52:38 -0500

    add "CrazyNewFeature" class. provides magical new functionality needs testing before merging with main branch

---
+++ GitTest/CrazyNewFeature.swift
@@ -0,0 +1,20 @@
+//
+//  CrazyNewFeature.swift
+//  GitTest
+//
+//  Created by Kody O'Connell on 10/13/15.
+//  Copyright © 2015 Kody O'Connell. All rights reserved.
+//
+
+import Foundation
+
+
+class CrazyNewFeature {
+    
+    let name = "best feature ever"
+    let amount = 1000
+    
+    func makeMagic() {
+        print("magic happening...")
+    }
+}
